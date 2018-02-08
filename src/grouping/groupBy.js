c8ddd0dc2e1d91a5e897056ba0a0c933de001076
mindrones <mindrones@users.noreply.github.com>
2018-02-09 00:47:00 +0100

    split grouping.js

---
+++ src/grouping/groupBy.js
@@ -0,0 +1,42 @@
+import {_curry2} from "../privates/currying";
+import group from "./group";
+
+/**
+ * A curried version of {@link module:lamb.group|group} that uses the provided iteratee
+ * to build a function expecting the array-like object to act upon.
+ * @example
+ * var persons = [
+ *     {"name": "Jane", "age": 12},
+ *     {"name": "John", "age": 40},
+ *     {"name": "Mario", "age": 18},
+ *     {"name": "Paolo", "age": 15}
+ * ];
+ *
+ * var getAgeStatus = function (person) { return person.age > 20 ? "over 20" : "under 20"; };
+ * var groupByAgeStatus = _.groupBy(getAgeStatus);
+ *
+ * var personsByAgeStatus = groupByAgeStatus(persons);
+ *
+ * // "personsByAgeStatus" holds:
+ * // {
+ * //     "under 20": [
+ * //         {"name": "Jane", "age": 12},
+ * //         {"name": "Mario", "age": 18},
+ * //         {"name": "Paolo", "age": 15}
+ * //     ],
+ * //     "over 20": [
+ * //         {"name": "John", "age": 40}
+ * //     ]
+ * // }
+ *
+ * @memberof module:lamb
+ * @category Array
+ * @function groupBy
+ * @see {@link module:lamb.group|group}
+ * @see {@link module:lamb.count|count}, {@link module:lamb.countBy|countBy}
+ * @see {@link module:lamb.index|index}, {@link module:lamb.indexBy|indexBy}
+ * @since 0.7.0
+ * @param {ListIteratorCallback} iteratee
+ * @returns {Function}
+ */
+export default _curry2(group, true);
